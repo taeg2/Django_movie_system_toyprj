3f167d2b6474c04092be743bf1e788f4b1e72a1f
TaeGyoung Lee <90079870+taeg2@users.noreply.github.com>
2025-09-21 11:19:09 +0000

    movie model 수정

--- movie_system/movie/models.py
+++ movie_system/movie/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class Movie(models.Model):
-    title = models.IntegerField()
-    runtime = models.IntegerField()
-    genre = models.CharField(max_length=30)
-    totalAudience = models.BigIntegerField()
+    title = models.CharField(max_length=100, default="")
+    runtime = models.IntegerField(default=0)
+    genre = models.CharField(max_length=100, default="")
+    totalAudience = models.BigIntegerField(default=0)
 
     def __str__(self):
         return self.title
 
 class Theater(models.Model):
-    name = models.CharField(max_length=20)
-    address = models.CharField(max_length=200)
+    name = models.CharField(max_length=100, default="")
+    address = models.CharField(max_length=100, default="")
     
     def __str__(self):
         return self.name
@@ -19,38 +19,49 @@ class Theater(models.Model):
 class Room(models.Model):
     theater = models.ForeignKey(Theater, on_delete=models.CASCADE)
 
-    room_number = models.IntegerField()
-    row_count = models.IntegerField()
-    col_count = models.IntegerField()
+    number = models.IntegerField(default=0)
+    type = models.CharField(max_length=100, default="")
+    row_max = models.IntegerField(default=0)
+    col_max = models.IntegerField(default=0)
 
     class Meta:
-        unique_together = ('theater', 'room_number')
+        unique_together = ('theater', 'number')
 
     def __str__(self):
-        return f"{self.theater.name} - {self.room_number}"
+        return f"{self.theater.name} - {self.number}"
     
 class Seats(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True)
 
-    row = models.IntegerField()
-    col = models.IntegerField()
+    row = models.IntegerField(default=0)
+    col = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('room', 'row', 'col')
 
     def __str__(self):
         return f"행: {self.row}, 열: {self.col}"
     
 class Screening(models.Model):
-    theater = models.ForeignKey(Theater, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    
+    price = models.IntegerField(default=0)
+    start_time = models.DateField()
+    discount_status = models.BooleanField(default=False)
 
-    starttime = models.DateField()
-    discountstatus = models.BooleanField()
+    def discount_price(self):
+        if self.is_discount: return self.price * 0.9
+
+    def is_discount(self):
+        start_hour = self.start_time.hour
+        return start_hour < 10 or start_hour > 22
 
     def __str__(self):
-        return f"{self.theater.name}\n{self.movie.title}\n{self.starttime}\n"
+        return f"{self.movie.title}\n{self.start_time}\n"
 
 class Reservation(models.Model):
     screening = models.ForeignKey(Screening, on_delete=models.CASCADE)
     seats = models.ForeignKey(Seats, on_delete=models.CASCADE)
 
-    booking_status = models.BooleanField()
-    discount_satus = models.BooleanField()
+    booking_status = models.BooleanField(default=False)
